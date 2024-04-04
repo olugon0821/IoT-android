40e9cdd1c5fd54019f3d03df5f513168f5f0844b
olugon <126735508+olugon0821@users.noreply.github.com>
2024-04-04 16:00:16 +0900

    Add files via upload

---
+++ ApplicationClass/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.applicationclass;
+
+import android.app.Application;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import android.widget.Button;
+import android.widget.EditText;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        editText = (EditText)findViewById(R.id.editText);
+        Button button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String id = editText.getText().toString();
+
+                Application app = getApplication();
+                MyApp myApp = (MyApp) app;
+                myApp.setId(id);
+
+                Intent intent = new Intent(MainActivity.this, SubActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ ApplicationClass/MyApp.java
@@ -0,0 +1,17 @@
+package com.example.applicationclass;
+import android.app.Application;
+import android.util.Log;
+public class MyApp extends Application{
+    private String id = null;
+    @Override
+    public void onCreate(){
+        super.onCreate();
+        Log.i("test","Application onCreate() called");
+    }
+    public String getId(){
+        return id;
+    }
+    public void setId(String id){
+        this.id = id;
+    }
+}
