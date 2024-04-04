b685fa8df09322db22ad8b3b7ecbafac657966d3
olugon <126735508+olugon0821@users.noreply.github.com>
2024-04-04 10:07:27 +0900

    Create Toast.java

---
+++ Toast/Toast.java
@@ -0,0 +1,47 @@
+package com.example.toadt;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    private ImageView imageView;
+    private EditText editText;
+    private  Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imageView = (ImageView) findViewById(R.id.imageView);
+        editText = (EditText) findViewById(R.id.editText);
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String msg = editText.getText().toString();
+                editText.setText("");
+                Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        imageView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                Toast.makeText(MainActivity.this, "Hello",
+                        Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
+    }
+}
